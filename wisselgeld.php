9f5ffe13dd8dd038ceae9bdd97a45d88fc1c2b3b
Josh <2107767@talnet.nl>
2019-12-04 14:22:10 +0100

    Uitwerking van de exercise

---
+++ wisselgeld.php
@@ -0,0 +1,15 @@
+<?php
+
+$data = intval($argv[1]);
+
+$array = array(10, 5, 2, 1);
+
+foreach($array as $value) {
+
+    $geld = floor($data / $value);
+    if ($geld >= 1) {
+        $data = $data - ($value * $geld);
+        echo $geld . " x " . $value . " euro " . PHP_EOL;
+    }
+} 
+
